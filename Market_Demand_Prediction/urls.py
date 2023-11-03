772971d6309534be50f1cca69f124d04eb641b6e
Yugapriya Shankar <shankary@uwindsor.ca>
2023-11-03 10:59:32 -0400

    Added banner and made small UI tweaks

--- Market_Demand_Prediction/urls.py
+++ Market_Demand_Prediction/urls.py
@@ -36,5 +36,5 @@ urlpatterns = [
     path('password-reset-complete/',
          auth_views.PasswordResetCompleteView.as_view(template_name='password_reset_complete.html'),
          name='password_reset_complete'),
-    #path('', BannerView.as_view(), name='banner'),
+    path('banner/', BannerView.as_view(), name='banner'),
 ]
